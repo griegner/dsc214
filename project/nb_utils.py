000bc97905e5598a85cafda41b70bff5732fa2bd
Gabriel Riegner <gabriel.riegner@gmail.com>
2024-06-10 22:23:56 -0700

    plot theoretical and estimated decision boundary

--- project/nb_utils.py
+++ project/nb_utils.py
@@ -3,15 +3,42 @@ from ripser import ripser
 from scipy import sparse
 
 
+def plot_stationarity_triangle(ax, fill_alpha=0.1):
+    """Plot stationarity triangle for an AR(2) process"""
+    ax.set_xlabel(r"$\phi_1$")
+    ax.set_ylabel(r"$\phi_2$")
+
+    # set x, y limits
+    ax.set_xticks([0, 1, 2])
+    ax.set_xlim([-0.1, 2.1])
+    ax.set_yticks([-1, 0, 1])
+    ax.set_ylim([-1.1, 1.1])
+
+    # plot AR(2) stationarity triangle
+    phi1 = np.linspace(0, 2, 100)
+    lwr, upr1, upr2 = -1, 1 + phi1, 1 - phi1
+    ax.fill_between(
+        phi1, np.maximum(lwr, np.minimum(upr1, upr2)), lwr, color="gray", alpha=fill_alpha
+    )
+    ax.plot(
+        phi1,
+        (-0.25 * phi1**2),
+        c="k",
+        ls="--",
+        lw=1.5,
+        label=r"theoretical: $\phi_2 = - \phi_1^2 / 4$",
+    )
+
+
 def gen_ar2_coeffs(oscillatory=False, phi1="both", random_seed=0):
     """generate coefficients for an stationary AR(2) process"""
     rng = np.random.default_rng(seed=random_seed)
     if phi1 == "positive":
-        phi1 = rng.uniform(0, 1.9)
+        phi1 = rng.uniform(0, 2)
     elif phi1 == "negative":
-        phi1 = rng.uniform(-1.9, 0)
+        phi1 = rng.uniform(-2, 0)
     else:
-        phi1 = rng.uniform(-1.9, 1.9)
+        phi1 = rng.uniform(-2, 2)
     if oscillatory:
         phi2 = rng.uniform(-1, -0.25 * phi1**2)
     else:
